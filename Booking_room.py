cfa8d9abc6c087e966c34bfeb6d14e22b751faa1
tz2614 <tony_zheng35@hotmail.com>
2019-06-12 11:08:00 +0100

    README.md changed to reflect the removal of Admin class

--- Booking_room.py
+++ Booking_room.py
@@ -15,17 +15,6 @@ import timeit
 
 """Create a OOP python class program for booking a room"""
 
-# create count for each class to keep track of the number of users, admin and rooms
-room_count = 0
-user_count = 0
-admin_count = 0
-booking_count = 0
-
-# create a dictionary for each class where info is stored
-user_info = {}
-admin_info = {}
-room_info = {}
-booking_info = {}
 
 class User(object):
 
@@ -37,6 +26,7 @@ class User(object):
 		password
 		name
 		status (e.g. admin or user)
+
 	Methods:
 		register
 		login
@@ -49,6 +39,15 @@ class User(object):
 		add_user
 	"""
 
+	# create user_count/admin_count to keep track of the number of users and admins
+
+	user_count = 0
+	admin_count = 0
+
+	# create a dictionary for each class where info is stored
+
+	user_info = {}
+
 	def __init__(self, username, password, name, status):
 
 		"""Return a new User object."""
@@ -57,13 +56,14 @@ class User(object):
 		self.password = password
 		self.name = name
 		self.status = status
-		user_info[self.username] = {}
-		user_info[self.username]["name"] = self.name
-		user_info[self.username]["password"] = self.password
-		if self.status = "admin":
-			user_count += 1
-		if self.status = "admin"
-			admin_count += 1
+		User.user_info[self.username] = {}
+		User.user_info[self.username]["name"] = self.name
+		User.user_info[self.username]["password"] = self.password
+		User.user_info[self.username]["status"] = self.status
+		if self.status == "admin":
+			User.user_count += 1
+		if self.status == "admin":
+			User.admin_count += 1
 
 	def login(self):
 
@@ -73,9 +73,9 @@ class User(object):
 		new_username = input("enter your username: ")
 		new_password = input("enter your password: ")
 
-		if new_username in user_info.keys() and new_password == user_info[new_username]:
+		if new_username in User.user_info.keys() and new_password == User.user_info[new_username]:
 			print ("login successful")
-			user_info["last_login_time"] = datetime.datetime.now()
+			User.user_info["last_login_time"] = datetime.datetime.now()
 			return True
 		else:
 			print ("username or password incorrect")
@@ -96,12 +96,12 @@ class User(object):
 
 		new_username = input("enter your username[e.g.johnsmith1:")
 
-		if new_username in user_info.keys():
+		if new_username in User.user_info.keys():
 			print ("username already exist, try another username")
 			return False
 
 		else:
-			user_info[new_username] = user_info[self.username]
+			User.user_info[new_username] = User.user_info[self.username]
 			print ("username entered successfully")
 			return True
 
@@ -111,10 +111,10 @@ class User(object):
 
 		new_password = input("new password:")
 
-		if new_password in user_info[self.username]["password"]:
+		if new_password in User.user_info[self.username]["password"]:
 			print ("password unchanged")
 		else:
-			user_info[self.username]["password"] = new_password
+			User.user_info[self.username]["password"] = new_password
 			print ("password changed")
 
 	def new_name(self):
@@ -123,19 +123,19 @@ class User(object):
 
 		new_name = input("enter your full name [e.g.john_smith, please use _ between first and last name]:")
 		
-		if new_name in user_info[self.username]["name"]:
+		if new_name in User.user_info[self.username]["name"]:
 			print ("name already exist, try another name")
 			self.new_name()
 		
 		else:
-			user_info[self.username]["name"] = new_name
+			User.user_info[self.username]["name"] = new_name
 			print ("new name entered successfully")
 
-	def displayinfo(self, username):		
+	def displayinfo(self):		
 
 		"""A user can view their user info after login"""
 
-		pp.pprint (user_info[username])
+		pp.pprint (User.user_info[self])
 
 class Room(object):
 
@@ -155,6 +155,9 @@ class Room(object):
 		set_status
 	"""
 
+	room_info = {}
+	room_count = 0
+
 	def __init__(self):
 		self.room = {}
 		self.status = None
@@ -167,48 +170,45 @@ class Room(object):
 
 		"""A user or administrator can set the room capacity"""
 
-		if room in room_info["room"]
+		if room in Room.room_info["room"]:
 			new_capacity = input("set room capacity [e.g.10]: ")
 
 			if new_capacity.isdigit():
 				self.capacity = new_capacity
-				room_info["room"]["capacity"] = new_capacity
+				Room.room_info["room"]["capacity"] = new_capacity
 			else:
 				print ("room capacity should be a number")
-				break
+
 		else:
 			print ("room name not found")
-			break
 
 	def set_equipment(self, room):
 
 		"""A user or administrator can set the equipment in the room"""
 
-		if room in room_info["room"]:
+		if room in Room.room_info["room"]:
 			new_equipment = input("enter name of equipment: ")
-			room_info["room"]["equipment"] = new_equipment
+			Room.room_info["room"]["equipment"] = new_equipment
 		else:
 			print ("room name not found")
-			break			
 
 	def set_location(self, room):
 
 		"""A user or administrator can set the location of the room"""
 
-		if room in room_info["room"]:
+		if room in Room.room_info["room"]:
 			new_location = input("enter room location: ")
-			room_info["room"]["location"] = new_location
+			Room.room_info["room"]["location"] = new_location
 		else:
 			print ("room name not found")
-			break			
 
 	def set_status(self):
 
 		"""A user or administrator can set the status of the room"""
 
-		if room in room_info["room"]:
+		if room in Room.room_info["room"]:
 			new_status = input("enter new room status [free/occupied]: ")
-			room_info["room"]["status"] = new_status
+			Room.room_info["room"]["status"] = new_status
 		else:
 			print ("room name not found")
 
@@ -216,9 +216,9 @@ class Room(object):
 
 		"""A user/administrator can view room info"""
 
-		pp.pprint (room_info[username])
+		pp.pprint (Room.room_info[username])
 
-class Booking(User, Admin, Room):
+class Booking(User, Room):
 
 	"""
 	A booking that can be made by either a user or administrator after they have logged in
@@ -241,19 +241,26 @@ class Booking(User, Admin, Room):
 		edit_booking
 	"""
 
+	booking_count = 0
+	booking_info = {}
+
 	def __init__(self, room, status, capacity, equipment, location, date, time, duration):
 		Room.__init__(self, room, status, capacity, equipment, location)
-		self.date = []
-		self.time = []
-		self.duration = []
-		booking_count += 1
+		self.date = date
+		self.time = date
+		self.duration = date
+		Booking.booking_info[self.room] = {}
+		Booking.booking_info[self.room]["date"] = self.date
+		Booking.booking_info[self.room]["time"] = self.time
+		Booking.booking_info[self.room]["duration"] = self.duration
+		Booking.booking_count += 1
 
 	def set_date(self, room):
 		
 		"""set a date for the meeting"""
 		print ("please enter the room you would like to book")
 
-		if self.room in booking_info["room"]:
+		if self.room in Booking.booking_info["room"]:
 
 			new_date = input("Enter a date for the meeting [YYYY-MM-DD]: ")
 
@@ -264,20 +271,19 @@ class Booking(User, Admin, Room):
 			year = int(new_date.split("/")[2])
 			
 			if day > 0 and month > 0 and year > 0:
-				booking_info["room"][self.room]["date"] = new_date
+				Booking.booking_info["room"][self.room]["date"] = new_date
 				print ("booking date set")
 			else:
 				print ("date entered contain negative numbers")
 		else:
 			print ("check format of date entered")
 
-		return booking_info
 
 	def set_time(self, room):
 
 		"""set a time for the meeting"""
 
-		if self.room in booking_info["room"]:
+		if self.room in Booking.booking_info["room"]:
 
 			new_time = input("Enter start time for the meeting [e.g.HH:MM, please use 24hour format]: ")
 
@@ -287,7 +293,7 @@ class Booking(User, Admin, Room):
 			minute = int(new_date.split(":")[1])
 
 			if 0 <= hour <= 23 and 0 <= minute < 60:
-				booking_info["room"][self.room]["time"] = new_time
+				Booking.booking_info["room"][self.room]["time"] = new_time
 				print ("booking time set")
 			else:
 				print ("hours and minutes entered incorrect")
@@ -298,11 +304,11 @@ class Booking(User, Admin, Room):
 
 		"""set duration of meeting"""
 
-		if self.room in booking_info["room"]:
+		if self.room in Booking.booking_info["room"]:
 			new_dur = input("Enter meeting duration in mins (e.g.30 or 60): ")
 
 		if new_dur.isdigit():
-			last_meet_time = booking_info["room"][self.room]["time"][-1]
+			last_meet_time = Booking.booking_info["room"][self.room]["time"][-1]
 			new_minute = int(last_meet_time.split(":")[-1]) + new_dur
 
 			if new_minute > 59:
@@ -313,9 +319,8 @@ class Booking(User, Admin, Room):
 				else:
 					pass
 			else:
-
-			booking_info["room"][self.room]["duration"]:
-			print ("booking duration set")
+				Booking.booking_info["room"][self.room]["duration"] = new_dur
+				print ("booking duration set")
 
 	"""
 	def remove_booking()
@@ -324,8 +329,8 @@ class Booking(User, Admin, Room):
 
 if __name__ == "__main__":
 
-	testuser = User(zhengt, abcde, tengyuezheng)
-	print (User.displayinfo(zhengt))
+	testuser = User("zhengt", "abcde", "tengyuezheng", "admin")
+	print (User.displayinfo("zhengt"))
 
 	#Room.set_equipment(room1)
 	#Booking.set_date(room1)
